7250a66042235fc4113db8a20f3b26ce234afed8
Dana <dana@factor1studios.com>
2017-12-21 14:07:58 -0700

    Edited 401k contribution post

--- education/401k-contributions.php
+++ education/401k-contributions.php
@@ -27,41 +27,65 @@
               <div class="span8 offset2">
 
                 <div class="education-article__content">
-                  <h1 class="education-article__heading">Benefits of Contributing to your 401(k)</h1>
+                  <h1 class="education-article__heading">Did You Know: Benefits of Contributing to your 401(k)</h1>
+
+                  <h3>The Missing Leg of the Three-Legged Stool of Retirement</h3>
+
+                  <p>The three-legged stool is a metaphor for how the post-World War II generation looked at planning for retirement. Each leg represented one portion of planned retirement income. The legs were: employer pension, employee savings, and Social Security. The idea was that you needed each one to build a strong retirement foundation. Without any one, the three-legged stool would fall apart. This metaphor is often attributed to Franklin D. Roosevelt, who created the Social Security program.</p>
+
+                  <p>The three-legged stool no longer exists, or, at the least, it looks a lot different. Pensions, or Defined Benefit Plans, that were once offered by most employers, and were a primary component of retirement planning, are rapidly vanishing. </p>
+
+                  <p>Many employers offer 401(k) Defined Contribution Retirement Plans instead. But since employees have to make contributions through their own paychecks, a 401(k) is more a part of the personal savings leg than the old employer pension leg. Fortunately, most airlines contribute considerably more than the average amount to 401k's, so there is still an employer leg for most people in the aviation industry, although it's a lot smaller and weaker than the old Defined Benefit or pension leg. For other employees there is usually an employer 'matching contribution', which is an even smaller and weaker leg.</p>
+
+                  <h3>Tax Benefits for 401k Contributions</h3>
+
+                  <p>Here's the good news. For the average pilot, for every $4 contribution to his or her 401k, the IRS gives back $1 in tax reduction. So, a $4 contribution only costs $3.</p>
+
+                  <p>ROTH 401ks are becoming increasingly popular. Although the benefits of a ROTH are far reaching, you need to ask yourself, will you be in a higher or lower tax bracket in retirement. For most of our aviation clients, you will be receiving less income in retirement so the traditional 401k might be a better choice in your high earning years.</p>
+
+                  <p>Bottom line – making traditional 401k contributions actually lowers your taxable income each year so that you pay less in taxes.</p>
+
+                  <h3>When Should I Start Contributing?</h3>
+
+                  <p>There are two primary variables which dictate what your ending 401k balance will be - average cumulative return and the total time of accumulation. The longer you contribute, the more you'll have to use in retirement. For young professionals, with little or no pension benefits, the answer to the above question is: If the time is now 9am, you should start making contributions before taking a bite of lunch.</p>
 
                   <h3>Should I max out my personal 401k contributions?</h3>
 
-<p>We are asked this question often. Here is our question: How many times would you give me seven bucks in return for ten? That’s about the average benefit you get in tax breaks – a senior left-seat driver will get more. The 401k contributions are before-tax dollars, so your W2 amount on which your tax is based, leaves your contribution out. So, in effect, for a $7 effective outlay you are getting the benefit of a $10 contribution.</p>
+                  <p>In the absence of a Defined Benefit or pension plan, the answer to the above question would be a resounding YES. Since you may have only two sources of income, young folks who have little or no residual PBGC pension benefits, absolutely MUST contribute as much as you possibly can, and max out if at all possible.</p>
 
-<p>NOTE: I’ve talked to a few folks who are under the impression that since there is a phase out of IRA contributions with increasing AGI amounts, the same is true for 401k contributions. Not so - there is no income limit on 401k contributions. So, even if you don’t get the IRA deduction, you do qualify for the 401k benefit.</p>
+                  <h3>How will contributing to my 401k affect my retirement?</h3>
 
-<p><u>So now you know that contributing to your 401k actually lowers your taxable income each year so you pay less in taxes</u>. Let’s look at what personal contributions do to portfolio values and income potential next.</p>
+                  <p>Let’s take a 30 year old FO, who is considering whether to make his or her own contributions to his Defined Contribution Plan, 401k, or just let the company contribute the contractual amount.</p>
 
-<p>Now let’s take a 40 year old FO, who begins at that age to contribute the max. By age 60, under typical investment conditions in a balanced portfolio, he or she would accumulate about <u>$740,000 additional in his or her 401k, over the amount he or she would have accumulated with just company contributions</u>. That amount may be enough to give him or her the option of early retirement. That amount could comfortably produce another 30k of income in retirement.</p>
+                  <p>The graph below summarizes projected ending conditions for his or her 401k at the pilot's retirement age, 65:</p>
 
-<p>By age 65, he or she would accumulate about $1.2 million additional. This would be an additional 54k of income in retirement.</p>
+                  <p>Assumptions:</p>
+                  <ul>
+                  <li>The pilot remains as FO for 10 years, then moves to the left seat at age 45.</li>
+                  <li>Average pay rates remain unchanged.</li>
+                  <li>The pilot's 401k is invested in a moderate, balanced portfolio.</li>
+                  <li>The IRS maximum contributions remain at the present level.</li>
+                  <li>Values are adjusted for historical inflation, to give projections in 'today's dollars'.</li>
+                  </ul>
 
-<p>Is that worth it? We think so.</p>
+                  <p><img src="../img/education/vol5-401(k)-values-chart.png" alt=""></p>
 
-<p>NOTE: The above calculations take into account current personal and employer contribution limits. These limits change over time.</p>
+                  <p>By age 65, under typical investment conditions in a balanced portfolio, he or she would accumulate about $1.7 mm +/- in the 401k from the company contributions, alone. (Bar in blue on left, measured on left Axis.)</p>
 
-<p>Here is a graphical display of the projected 401(k) balances and possible income achieved from those portfolio values. We used an United Pilot as an example but these numbers will be very similar across all carriers.</p>
+                  <p>Using the 4% Rule of Thumb for 'withdrawals under uncertainty', that amount would provide $68,000 +/- in income to the pilot during retirement years. (Red dot at top of blue bar, measured on right Axis.) Adding Social Security income would bring the total to $100,000 +/-.</p>
 
-<p><img src="../img/education/graph-401k.png" alt=""></p>
+                  <p>IMPORTANT: Now, let’s look at what personal contributions do to portfolio values and income potential. By contributing the max each year (FYI, the max employee contribution for 2017 is $18,000. If you are over 50, $24,000), the 401k value would increase to $3.1 mm +/-. (Blue bar on right.) The income provided, again using the 4% rule, would amount to $124,000 +/-. Adding Social Security, total income is about $156,000.</p>
 
-<p>The above chart compares the hypothetical values of a PRAP portfolio and the spending that would be available from a pilot’s PRAP account accumulation:</p>
+                  <p>In summary, personal contributions to your 401k should be right up there near the top of your financial priority list. Is there anything that should be above it in priority? Absolutely – credit card and high interest consumer debt, should be the first thing you tackle. Once your debt is under control, get on-line immediately and adjust your 401k contributions as high as possible.</p>
 
-<ul>
-<li>UAL ONLY - a typical pilot’s portfolio and available spending at age 60, with only UAL contributions to his PRAP.</li>
-<li>UAL + Pilot at age 60 – a typical pilot’s portfolio and available spending with company contributions PLUS his or her maximum PRAP contributions.</li>
-<li>UAL + Pilot at age 65 – a typical pilot’s portfolio and available spending with company contributions PLUS his or her maximum PRAP contributions, PLUS the benefit of additional compounding of the PRAP account gained by waiting until age 65 to retire.</li>
-</ul>
+                  <blockquote>
+                  <p>“Compound interest is the eighth wonder of the world. He who understands it, earns it. He who doesn’t, pays it.”
+                  <cite>Albert Einstein</cite></p>
+                  </blockquote>
 
-<p>NOTE: Social Security and PBGC benefits are disregarded in spending amounts.</p>
+                  <h3>Strategy</h3>
 
-<p>In summary, personal contributions to your 401k should be right up there near the top of your financial priority list. Is there anything that should be above it in priority? Absolutely – credit card and high interest consumer debt, (i.e. any double digit interest debt), should be the first thing you take out. But as soon as that’s done, get on line and adjust your 401k contributions as high as possible.</p>
-
-<p>Call us at the office if you have any questions.</p>
+                  <p>Now that you see how important making your OWN contributions can be towards a successful retirement, a reasonable way to start making contributions if you aren’t doing so already or to try to max out employee contributions is to set up an automatic increase each year. You get a raise almost every year you work. Therefore, increase your contribution rate to the 401k at the same time you get your raise every year. Say you are contributing 5% of your salary currently, next year raise it to 7%, the following year move it to 9%, etc. You will barely notice that your check has changed and you are on your way to maxing contributions over time and putting yourself in good position for a very enjoyable retirement!</p>
 
                 </div>
               </div>
@@ -74,47 +98,47 @@
 
                     <h4 class="education-article__related-heading">Continue Learning</h4>
                     <ul class="education-article__related-list">
-                      
+
                         <li class="education-article__related-list-item">
                           <a class="education-article__related-link " href="/education/gold-making-headlines-again.php" title="">Gold Making Headlines Again</a>
                         </li>
-                      
+
                         <li class="education-article__related-list-item">
                           <a class="education-article__related-link " href="/education/risk-reduction.php" title="">Risk Reduction in a Bear Market can Drive Positive Long-Term Results</a>
                         </li>
-                      
+
                         <li class="education-article__related-list-item">
                           <a class="education-article__related-link " href="/education/investors-investment-return.php" title="">Why is the average investor's investment return so low?</a>
                         </li>
-                      
+
                         <li class="education-article__related-list-item">
                           <a class="education-article__related-link " href="/education/social-security-benefits.php" title="">When Will You Begin Taking Social Security Benefits?</a>
                         </li>
-                      
+
                         <li class="education-article__related-list-item">
                           <a class="education-article__related-link " href="/education/retirement-you-want.php" title="">Can You Have the Retirement You Want?</a>
                         </li>
-                      
+
                         <li class="education-article__related-list-item">
                           <a class="education-article__related-link " href="/education/401k-volatility.php" title="">Did you know: 401K turbulence, what does it mean?</a>
                         </li>
-                      
+
                         <li class="education-article__related-list-item">
                           <a class="education-article__related-link " href="/education/insurance-beneficiary.php" title="">Would You Like To Be The Beneficiary Of Your Own Life Insurance Policy While You Are Living?</a>
                         </li>
-                      
+
                         <li class="education-article__related-list-item">
                           <a class="education-article__related-link " href="/education/retirement-rhra.php" title="">Retirement Health Reimbursement Arrangement (RHRA)</a>
                         </li>
-                      
+
                         <li class="education-article__related-list-item">
                           <a class="education-article__related-link " href="/education/roth-401k.php" title="">Roth 401(k) Contributions vs. Traditional 401(k) Contributions</a>
                         </li>
-                      
+
                         <li class="education-article__related-list-item">
                           <a class="education-article__related-link  current " href="/education/401k-contributions.php" title="">Benefits of Contributing to your 401(k)</a>
                         </li>
-                      
+
                     </ul>
 
                   </div>
